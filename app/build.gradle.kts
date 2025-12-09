59ccf2932736066796f8b0b148ca3040ceb2da10
yusufyilmaz00 <yusufyyilmaz0@gmail.com>
2025-12-09 23:55:56 +0300

    update: yeni dependency eklendi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -105,4 +105,22 @@ dependencies {
     implementation("io.ktor:ktor-client-cio:3.0.0")
     // login
     implementation(platform("io.github.jan-tennert.supabase:bom:3.0.1"))
-    implementation("io.github.jan-tennert.supabase:auth-kt")}
+    implementation("io.github.jan-tennert.supabase:auth-kt")
+    implementation("io.github.jan-tennert.supabase:serializer-kotlinx")
+    // coil
+    implementation("io.coil-kt:coil-compose:2.7.0")
+    // Paging 3 Runtime
+    implementation("androidx.paging:paging-runtime:3.3.0")
+    // Paging Compose
+    implementation("androidx.paging:paging-compose:3.3.0")
+    //
+    // Retrofit + Kotlinx Serialization
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+
+    // OkHttp
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+}
