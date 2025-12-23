43ef02020826d331402e26e670b3ae91140176df
yusufyilmaz00 <yusufyyilmaz0@gmail.com>
2025-12-23 16:58:17 +0300

    update: UserState modeline kullanıcı rolü eklendi

--- app/src/main/java/com/unluckyprayers/associumhub/data/local/model/UserState.kt
+++ app/src/main/java/com/unluckyprayers/associumhub/data/local/model/UserState.kt
@@ -3,6 +3,9 @@ package com.unluckyprayers.associumhub.data.local.model
 sealed class UserState {
     object Idle : UserState()
     object Loading : UserState()
-    data class Success(val message: String) : UserState()
+    data class Success(
+        val message: String,
+        val role: String = "standard_user"
+    ) : UserState()
     data class Error(val message: String) : UserState()
 }
